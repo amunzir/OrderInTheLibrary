88add89fd65f471064230205dcc780a7fbfd2ff9
Aamina Munzir <aaminamunzir@aaminas-macbook-air.local>
2019-02-07 12:14:13 -0600

    working on level select VC

--- OrderInTheLibrary/LevelSelectVC.swift
+++ OrderInTheLibrary/LevelSelectVC.swift
@@ -10,13 +10,35 @@ import UIKit
 
 class LevelSelectVC: UIViewController {
 
+    @IBOutlet weak var lvl1Image: UIImageView!
+    @IBOutlet weak var lvl2Image: UIImageView!
+    @IBOutlet weak var lvl3Image: UIImageView!
+    @IBOutlet weak var lvl4Image: UIImageView!
+    @IBOutlet weak var lvl5Image: UIImageView!
+    @IBOutlet weak var lvl6Image: UIImageView!
+    @IBOutlet weak var lvl7Image: UIImageView!
+    @IBOutlet weak var lvl8Image: UIImageView!
+    @IBOutlet weak var lvl9Image: UIImageView!
+    @IBOutlet weak var lvl10Image: UIImageView!
+    @IBOutlet weak var lvl11Image: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func sortingBtnPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "sortingSegue", sender: nil)
+    }
+    @IBAction func shelvingBtnPressed(_ sender: UIButton) {
+    }
+    @IBAction func reorderingBtnPressed(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
